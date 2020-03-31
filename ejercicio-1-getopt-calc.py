51e7e019481117e38132400a6d59098e6a5af3a6
Esteban Rivas <estebanarivasv@gmail.com>
2020-03-31 12:18:42 -0300

    Excercise 1 file created, .gitignore modified, README.md reloctard to practice-excercises

---
+++ ejercicio-1-getopt-calc.py
@@ -0,0 +1,15 @@
+"""
+--- Exercise statement: N°1 - Getopt - Calculator
+
+If wanted, you can look in the examples folder Practice
+
+Crear una calculadora, donde se pase como argumentos luego de la opción -o el
+operador que se va a ejecutar (+,-,*,/), luego de -n el primer número de la operación,de -m el segundo número.
+Ejemplo:
+            ./calc -o + -n 5 -m 6
+            5 + 6 = 11
+Considerar que el usuario puede ingresar los argumentos en cualquier orden. El programa deberá verificar que los
+argumentos sean válidos (no repetidos, números enteros, y operaciones válidas.
+
+"""
+
