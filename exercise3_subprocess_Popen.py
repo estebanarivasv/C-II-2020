d74b0d749ba2ccdbbb8033be060147dd62351ae1
Esteban Rivas <estebanarivasv@gmail.com>
2020-04-06 15:43:22 -0300

    exercise3 uploaded

---
+++ exercise3_subprocess_Popen.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+import getopt
+import sys
+import subprocess as sp
+
+"""
+
+--- Exercise statement: N°3 - subprocess.Popen
+
+If wanted, you can look in the examples folder /practice-excercises/python/proc
+
+Escribir un programa que reciba por argumentos de línea de comandos los siguientes modificadores:
+
+    -c command
+    -f output_file
+    -l log_file
+
+El código deberá crear los archivos pasados por los argumentos -f y -l en el caso de que no existan.
+
+El código deberá ejecutar el comando haciendo uso de subprocess.Popen, y almacenar su salida en el
+archivo pasado en el parámetro -f. En el archivo pasado por el modificador -l deberá almacenar el
+mensaje “fechayhora: Comando XXXX ejecutado correctamente” o en su defecto el mensaje de error generado
+por el comando si este falla.
+
+Por ejemplo:
+
+python ejecutor.py -c “ip a” -f /tmp/salida -l /tmp/log
+
+El archivo /tmp/salida deberá contener la salida del comando, y /tmp/log deberá contener:
+
+fechayhora: Comando “ip a” ejecutado correctamente.
+
+Otro ejemplo:
+
+python ejecutor.py -c “ls /cualquiera” -o /tmp/salida -l /tmp/log
+
+El archivo /tmp/salida no contendrá nada nuevo, ya que el comando fallará. El archivo /tmp/log contendrá:
+
+fechayhora: ls: cannot access '/cualquiera': No such file or directory
+
+Notas
+
+    fechayhora debe ser la fecha y la hora del sistema en el momento de ejecutar el comando.
+    Si los archivos ya tienen contenido, las nuevas ejecuciones agregarán mensajes al final de los mismos, 
+sin limpiar salidas anteriores.
+    Los mensajes de error almacenados en el log_file serán los mensajes que genera el comando en su intérprete de
+comandos, no deberá generarlos el progama escrito en python.
+
+"""
+
+
+def get_command(opts):
+    for i in opts:
+        for (op, arg) in opts:
+            if op == "-c":
+                return arg
+
+
+def get_log(opts):
+    for i in opts:
+        for (op, arg) in opts:
+            if op == "-l":
+                return arg
+
+
+def get_out_file(opts):
+    for i in opts:
+        for (op, arg) in opts:
+            if op == "-f":
+                return arg
+
+
+def main():
+
+    (opts, args) = getopt.getopt(sys.argv[1:], "f:l:c")
+    print(opts)
+    print(args)
+
+    """command = get_command(opts)
+    log = open(str(get_log(opts)), "a")
+    out_file = open(str(get_out_file(opts)), "a")
+
+    date_fn = sp.Popen(["date"], stdout=sp.PIPE, universal_newlines=True)
+    out = str(date_fn.communicate()[0])
+    message = "\n" + out + "Command " + str(command) + " properly executed.\n\n"
+
+    main_fn = sp.Popen([str(command)], stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+    print("main", main_fn.communicate())
+    (main_out, main_err) = main_fn.communicate()
+
+    if len(str(main_err)) == 0:
+        log.write(message)
+        out_file.write(str(main_out))
+    else:
+        log.write(str(main_err))"""
+
+
+if __name__ == '__main__':
+    main()
