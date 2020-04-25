774784a715da67d175fba0167884520ebf229794
Esteban <estebanarivasv@gmail.com>
2020-04-25 19:45:49 -0300

    Exercise 6 file created

---
+++ exercise6.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import getopt
+import sys
+import os
+
+"""
+
+--- Exercise statement: N°5 - fork
+
+Realice un programa que genere N procesos hijos. Cada proceso al iniciar deberá mostrar:
+
+“Soy el proceso N, mi padre es M” N será el PID de cada hijo, y M el PID del padre.
+
+             padre
+
+          /   |   \
+
+    hijo1   hijo2   hijo3
+
+La cantidad de procesos hijos N será pasada mediante el argumento "-n" de línea de comandos.
+
+"""
+
+
+def child_speaking():
+    print("Soy el proceso ", os.getpid(), "y mi padre es", os.getppid())
+    os._exit(0)
+
+
+def main():
+    chld_num = 0
+
+    (opts, args) = getopt.getopt(sys.argv[1:], "n:")
+    for (opt, arg) in opts:
+        if opt == "-n":
+            chld_num = int(arg)
+            print(chld_num)
+
+    for i in range(chld_num):
+        new_chld = os.fork()
+        if new_chld == 0:
+            child_speaking()
+
+
+if __name__ == '__main__':
+    main()
