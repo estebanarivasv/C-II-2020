5f03284f0f6dc4afcd81207c8ff648e2ad63d850
Esteban <estebanarivasv@gmail.com>
2020-04-26 12:29:12 -0300

    Having trouble with this code.

--- exercise7_ping_pong.py
+++ exercise7_ping_pong.py
@@ -5,13 +5,15 @@ import signal
 
 """
 
---- Exercise statement: N°6 - ping_pong
+--- Exercise statement: N°7 - ping_pong
 
 Realice un programa que genere dos procesos.
 
-El proceso hijo1 enviará la señal SIGUSR1 al proceso padre y mostrará la cadena "Soy el hijo1 con PID=XXXX: ping" cada 5 segundos.
+El proceso hijo1 enviará la señal SIGUSR1 al proceso padre y mostrará la cadena "Soy el hijo1 con PID=XXXX: ping" 
+cada 5 segundos. 
 
-El proceso padre, al recibir la señal SIGUSR1 enviará esta misma señal al proceso hijo2. El proceso hijo2, al recibir dicha señal mostrará la cadena "Soy el hijo2 con PID=YYYY: pong" por pantalla.
+El proceso padre, al recibir la señal SIGUSR1 enviará esta misma señal al proceso hijo2. El proceso hijo2, 
+al recibir dicha señal mostrará la cadena "Soy el hijo2 con PID=YYYY: pong" por pantalla. 
 
 Este comportamiento se deberá detener a las 10 señales enviadas por el proceso hijo1.
 
@@ -27,29 +29,37 @@ Soy proceso hijo2 con PID=1547: "pong"
 """
 
 
-def handler_hijo(s, f):
-    print("My parent PID %d sent me a SIGUSR1 signal." % os.getppid())
+def USR1_chld_handler(signal, frame):
+    print("I'm the child No. 2\tPID: ", os.getpid(), ": pong")
 
 
-def handler_padre(s, f):
-    print("Goodbye! Finishing execution.")
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+def USR1_parent_handler(signal, frame):
+    pass
 
 
 def main():
-    pid = os.fork()
+    child1 = os.fork()
+    child2 = os.fork()
 
-    if pid == 0:
-        signal.signal(signal.SIGUSR1, handler_hijo)
+    # CHILD PROCESS No. 1
+    if child1 == 0:
+        for i in range(10):
+            print("I'm the child No. 1\tPID: ", os.getpid(), ": ping")
+            os.kill(int(os.getppid()), signal.SIGUSR1)
+            time.sleep(5)
+
+    # CHILD PROCESS No. 2
+    if child2 == 0:
+        signal.signal(signal.SIGUSR1, USR1_chld_handler)
         while True:
             signal.pause()
+
+    # PARENT PROCESS
     else:
-        signal.signal(signal.SIGINT, handler_padre)
-        for i in range(10):
-            time.sleep(10)
-            os.kill(pid, signal.SIGUSR1)
-        print("Parent killing son.")
-        os.kill(pid, signal.SIGTERM)
+        signal.signal(signal.SIGUSR1, USR1_parent_handler)
+        while True:
+            signal.pause()
+            os.kill(child2, signal.SIGUSR1)
 
 
 if __name__ == '__main__':
