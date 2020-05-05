c8eb4e340c94f8452315b28925ea0981d75a8eb0
Esteban <estebanarivasv@gmail.com>
2020-05-05 14:13:05 -0300

    Exercise 9 and 10 statements created

---
+++ exercise10.py
@@ -0,0 +1,32 @@
+import sys
+import getopt
+import os
+import signal
+import time
+
+"""
+
+--- Exercise statement: N°10 - 
+
+Crear un sistema Productor-Consumidor (Escritor-Lector) donde un proceso productor almacene un mensaje de saludo en una 
+tubería FIFO. Ese mensaje será enviado mediante línea de comandos como argumento del programa. Ejemplo
+
+./saludofifo "HOLA MUNDO"
+
+Otro programa (consumidor) deberá leer el mensaje desde la tubería FIFO, generar un proceso hijo (fork) y enviarle por 
+PIPE el mensaje al hijo.
+
+El proceso hijo mostrará por pantalla el mensaje recibido.
+
+Proc1_fifo_escritor → FIFO → Proc2_fifo_lector → pipe → Proc2hijo
+
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
