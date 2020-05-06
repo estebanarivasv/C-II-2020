cf443717df71476347d0bbfe57a7082cb6e6502f
Esteban <estebanarivasv@gmail.com>
2020-05-06 09:01:05 -0300

    Exercise 10 fully working. WAY MORE EASIER THAN ex9 xD

--- exercise10_consumer.py
+++ exercise10_consumer.py
@@ -25,7 +25,33 @@ Proc1_fifo_escritor → FIFO → Proc2_fifo_lector → pipe → Proc2hijo
 
 
 def main():
-    pass
+    pipe_output = '/tmp/greeting'
+
+    lines = ""
+
+    # Consumer gets the greeting
+    fifo = open(pipe_output, 'r')
+    lines = fifo.readlines()
+    fifo.close()
+
+    r, w = os.pipe()
+    child = os.fork()
+
+    if not child:
+        # Child sentences, reads greeting from pipe
+        os.close(w)
+        read_head = open(r, 'r')
+        greeting = read_head.readlines()
+        print("Producer sends greetings: ")
+        for line in greeting:
+            print(line)
+    else:
+        # Parent sentences, writes greeting in pipe
+        os.close(r)
+        write_head = open(w, 'w')
+        write_head.writelines(lines)
+        write_head.flush()
+        write_head.close()
 
 
 if __name__ == '__main__':
--- exercise10_producer.py
+++ exercise10_producer.py
@@ -25,7 +25,18 @@ Proc1_fifo_escritor → FIFO → Proc2_fifo_lector → pipe → Proc2hijo
 
 
 def main():
-    pass
+    pipe_input = '/tmp/greeting'
+
+    greeting = sys.argv[1:]
+
+    fifo = open(pipe_input, "w")
+    for line in greeting:
+        fifo.write(line)
+    fifo.flush()
+    fifo.close()
+    print("Producer says: ")
+    for line in sys.argv[1:]:
+        print(line)
 
 
 if __name__ == '__main__':
