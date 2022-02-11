fc3bedda38d47f0abf79b8f6e4da05d4bae07960
Esteban Rivas <estebanarivasv@gmail.com>
2022-02-11 15:25:33 -0300

    Fix: orm was not reconnecting

--- help_chat/server/main/config/database.py
+++ help_chat/server/main/config/database.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 load_dotenv()
 DB_URI = os.getenv('DB_URI')
 
-engine = create_engine(DB_URI, echo=True, future=True)
+engine = create_engine(DB_URI, echo=True, future=True, pool_recycle=5, pool_timeout=10)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
