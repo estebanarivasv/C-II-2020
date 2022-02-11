917f47368c96842ca05bfb7ab9831203aaf3ebf5
Esteban Rivas <estebanarivasv@gmail.com>
2022-02-10 22:35:18 -0300

    Refactor: Move session from controller to config

--- help_chat/server/main/config/__init__.py
+++ help_chat/server/main/config/__init__.py
@@ -1,2 +1,4 @@
 from main.config.database import Base
 from main.config.database import engine
+from main.config.database import session
+
--- help_chat/server/main/config/database.py
+++ help_chat/server/main/config/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
@@ -8,3 +8,6 @@ DB_URI = os.getenv('DB_URI')
 
 engine = create_engine(DB_URI, echo=True, future=True)
 Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()
