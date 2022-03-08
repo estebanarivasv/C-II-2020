a3fbe816a7f994eaac6d41126573ce088da891a4
Esteban Rivas <estebanarivasv@gmail.com>
2022-03-08 11:21:29 -0300

    Refactor: minor changes

---
+++ help_chat/server/sample_operators.sql
@@ -0,0 +1,4 @@
+INSERT INTO authorized_operators.operator (name,username,password,department) VALUES
+	 ('Mónica Sanchez','monica.sanchez','123456','technical'),
+	 ('Eduardo Gómez','eduardo.gomez','123456','administrative'),
+	 ('Estela Moreno','estela.moreno','123456','sales');
