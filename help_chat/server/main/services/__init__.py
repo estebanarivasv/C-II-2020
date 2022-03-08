7b833a6a696b0c4564f8ee894e1094d1732464ed
Esteban Rivas <estebanarivasv@gmail.com>
2022-03-08 11:43:57 -0300

    Feat: change to threads to use less resources

--- help_chat/server/main/services/__init__.py
+++ help_chat/server/main/services/__init__.py
@@ -1,5 +1,4 @@
 from main.services.server import ServerService
 from main.services.chat import ChatService
-from main.services.chat_room import ChatRoomService
 from main.services.queue import QueueService
 
